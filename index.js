06607f184cecf97ceed5a237e95e5fc2684c02fd
John Tuttle <thejwt@gmail.com>
2017-12-07 16:58:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const wordCountMap = monologueLines.map( function(line){
   }, 0)
 
   return Object.assign({},line,{
-    [length_string]: wordArray.length
+    [length_string]: tempArray
   })
 })
 
